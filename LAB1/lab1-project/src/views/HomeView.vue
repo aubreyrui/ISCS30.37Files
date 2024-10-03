dd4be7afefede78e36a800d6e58905f49d8387e6
Aubrey San Jose <mary.sanjose@student.ateneo.edu>
2024-10-03 16:16:54 +0800

    added node.js and put vue stuff

--- LAB1/lab1-project/src/views/HomeView.vue
+++ LAB1/lab1-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import TheForm from '../components/Form.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <TheForm />
   </main>
 </template>
