61c792b2e090458fefc3430b7a6b5d86f413f15f
Aubrey San Jose <mary.sanjose@student.ateneo.edu>
2024-11-07 08:51:44 +0800

    initializing for LAB3

---
+++ LAB3/lab2-vue/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ClickerDos :label="name"/>
+    <ul>
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+    </ul>
+
+    <input type="text" name="name" id="name" v-model="name">
+    <p>Name: {{ name }}</p>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+import ClickerDos from '@/components/ClickerDos.vue'
+
+const name = ref('')
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }, {message: 'bro'}])
+</script>
---
+++ LAB3/lab2-vue/src/views/JeopardyView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+      <h1>This is NOT about page</h1>
+      <Jeopardy />
+    </div>
+  </template>
+  
+  <style scoped>
+  /* CSS */
+  
+  </style>
+  
+  <script setup>
+
+  import Jeopardy from '@/components/Jeopardy.vue'
+
+  </script>
